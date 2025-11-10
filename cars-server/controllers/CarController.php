19c706635de15b60217a1f62867a72b9db2e1277
Mohmmad Rostom <mohammadrstm2004@gmail.com>
2025-11-11 00:56:24 +0200

    added all crud operations of car model

--- cars-server/controllers/CarController.php
+++ cars-server/controllers/CarController.php
@@ -23,9 +23,56 @@ function getCars(){
     return;
 }
 
-//getCarById();
-getCars();
+function createCar(){
+    global $connection;
+    $data = json_decode(file_get_contents("php://input"), true);
+    if(isset($data)){
+        if(isset($data['car'])){
+            $newCarData = $data['car'];
+            if (!isset($newCarData['name'], $newCarData['year'], $newCarData['color'])) {
+                echo ResponseService::response(400, "Missing required car fields");
+                return;
+            }
+            $insterted_id = Car::create($connection,$newCarData);
+            echo ResponseService::response(200,$insterted_id);
+        }else{
+            echo ResponseService::response(401,"Missing car's data");
+        }
+    }else{
+        echo ResponseService::response(401,"Bad http request");
+    }
+}
 
+function updateCar(){
+    global $connection;
+    $data = json_decode(file_get_contents("php://input"), true);
+    if(isset($data)){
+        if(isset($data['car'])){
+            $updatedCarData = $data['car'];
+            if(Car::update($connection,$updatedCarData)){
+                echo ResponseService::response(200,$updatedCarData);
+            }else{
+                echo ResponseService::response(500,"Server error");
+            }            
+        }else{
+            echo ResponseService::response(401,"Missing car's data");
+        }
+    }else{
+        echo ResponseService::response(401,"Bad http request");
+    }
+}
 
-
+function deleteCar(){
+    global $connection;
+    if(isset($_GET["id"])){
+        $deleteId = $_GET["id"];
+        if(Car::delete( $connection ,$deleteId)){
+            echo ResponseService::response(200,$deleteId);
+        }else{
+            echo ResponseService::response(500,"Server error");
+        }            
+    }else{
+        echo ResponseService::response(401,"Bad http request");
+    }
+}
 ?>
--- cars-server/models/Model.php
+++ cars-server/models/Model.php
@@ -23,17 +23,44 @@ abstract class Model{
         $query = $connection->prepare($sql);
         $query->execute();
 
-        $data = $query->get_result()->fetch_assoc();
-        $cars  = [];
-        if($data){
-            foreach($data as $car){
-                $cars[] = new static($car);
-            }
-            return $cars;
+        $cars = [];
+        while($row = $query->get_result()->fetch_assoc()){
+            $cars[] = new static($row);
+        }
+        return $cars;
+    }
+
+    public static function create(mysqli $connection , $carData){
+        $sql = sprintf("INSERT INTO %s (name,year,color) VALUES(? , ? , ?)" , static::$table);
+        $query = $connection->prepare($sql);
+        $query->bind_param("sis", $carData["name"] , $carData["year"],$carData["color"]);
+        $query->execute();
+
+        return $query->insert_id;
+    }
+
+    public static function update(mysqli $connection , $updatedData){
+        $sql = sprintf("UPDATE %s SET name = ? , year = ? , color = ? WHERE id = ?" , static::$table);
+        $query = $connection->prepare($sql);
+        $query->bind_param("sisi", $updatedData["name"] , $updatedData["year"],$updatedData["color"],$updatedData["id"]);
+        if(!$query->execute()){
+            return false;
         }else{
-            return null;
+            return true;
         }
     }
+
+    public static function delete(mysqli $connection , $id){
+        $sql = sprintf("DELETE FROM %s WHERE id = ?" , static::$table);
+        $query = $connection->prepare($sql);
+        $query->bind_param("i" , $id);
+        if(!$query->execute()){
+            return false;
+        }
+        return true;
+    }
+
+
 
 }
 
