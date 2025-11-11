a8b2a46afeaaf97875b994caf56763e9a21e3901
Omarmhmdd <omarsamirmohamad@gmail.com>
2025-11-11 21:12:19 +0200

    code

--- cars-server/controllers/CarController.php
+++ cars-server/controllers/CarController.php
@@ -3,24 +3,58 @@ include("../models/Car.php");
 include("../connection/connection.php");
 include("../services/ResponseService.php");
 
-function getCarByID(){
+function getCars(){
     global $connection;
 
     if(isset($_GET["id"])){
         $id = $_GET["id"];
-    }else{
-        echo ResponseService::response(500, "ID is missing");
-        return;
-    }
-
-    $car = Car::find($connection, $id);
+        $car = Car::find($connection, $_GET["id"]);
     echo ResponseService::response(200, $car->toArray());
     return;
+    }else{
+        $car=Car::findAll($connection);
+        $allcars=[];
+        foreach($car as $row){
+           $allcars[]=$row->toArray();
+        }
+    echo ResponseService::response(200,$allcars);
+    return;
+    }
+
+    
 }
 
 //getCarById();
-getCars();
+//getCars();
 
+function insertCar(string $name, string $color, string $year){
+    global $connection;
+     Car::create($connection,$name, $color, $year);
+    echo ResponseService::response(200,"Table Created");
+}
+
+//insertCar('tesla','black','2010');
+
+
+   function updateCar(string $name, string $color, string $year){
+    global $connection;
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }
+    Car::update($connection,$id,$name,$color,$year);
+    echo ResponseService::response(200,"Row is Updated");
+}
+//updateCar("jeep", "white", "2000");
+
+function deleteCar(){
+    global $connection;
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+    }
+    Car::delete($connection, $id);
+    echo ResponseService::response(200,"row is Deleted");
+}
+deleteCar();
 //ToDO: 
 //transform getCarByID to getCars()
 //if the id is set? then we retrieve the specific car 
