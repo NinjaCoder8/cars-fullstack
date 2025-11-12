8369e99a18bc5d646ececeb53882053368a7172f
Jad Abdallah <abdullahjad3@gmail.com>
2025-11-13 01:34:42 +0200

    Added different CRUD functions

--- cars-server/models/Model.php
+++ cars-server/models/Model.php
@@ -30,6 +30,49 @@ abstract class Model{
         return $arr;
          
     }
+    public static function create(mysqli $connection,array $cardata){
+        $columns=array_keys($cardata);
+        $vals=array_values($cardata);
+        $placeHolders=implode(',',array_fill(0,count($columns),'?'));
+        $sql=sprintf("INSERT INTO %s (%s) VALUES (%s)",static::$table,implode(',',$columns,$placeHolders));
+        $statement=$connection->prepare($sql);
+        $types='';
+        foreach($vals as $val){
+            if (is_int($val)) $types .='i';
+            if (is_float($val)) $types .='d';
+            if (is_string($val)) $types .='s';
+            else $types .='b';
+        }
+        $statement=bind_param($types, ...$vals);
+        $query=$statement->execute();
+        return $query;
+    }
+    public static function update(mysqli $connection,int $id, array $cardata){
+        $columns=array_keys($cardata);
+        $vals=array_values($cardata);
+        $set=implode(' = ?, ',$columns) . '= ?';
+        $sql=sprintf("UPDATE %s WHERE %s = ?",static::$table,$set,static::$primary_key);
+        $statement=$connection->prepare($sql);
+        $types='';
+        foreach($vals as $val){
+            if (is_int($val)) $types .='i';
+            if (is_float($val)) $types .='d';
+            if (is_string($val)) $types .='s';
+            else $types .='b';
+        }
+        $types .= 'i';
+        $vals[]=$id;
+        $statement->bind_param($types, ...$vals);
+        $query=$statement->execute();
+        return $query;
+    }
+    public static function delete(mysqli $connection, int $id){
+        $sql=sprintf("DELETE FROM %s WHERE %s = ?",static::$table,static::$primary_key);
+        $statement=$connection->prepare($sql);
+        $statement->bind_param("i",$id);
+        $query=$statement->execute();
+        return $query;
+    }
 }
 
 
